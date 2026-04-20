fa8dca77fabe2b9576b43ab2e40cfef30535c83a
jcpenfie <juancarlospfierro@gmail.com>
2026-04-20 19:29:44 +0200

    importación faltante

--- app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.kt
+++ app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
